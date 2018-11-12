f5b163444e5752c70edf310bdad63d29450a29a9
Fizza Arif <farif@hawk.iit.edu>
2018-11-11 20:40:33 -0600

    Add querySelector and Event Listener to access doorbell with javascript

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,6 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+document.querySelector("#container").innerHTML = "<div id='newPlayButton'></div>"
+document.querySelector("#newPlayButton").addEventListener("click",ringDoorbell)
